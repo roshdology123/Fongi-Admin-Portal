40956ea1c6b98187fd03bd8c1ddeb32e7b237d69
Abdallah Roshdy <67377062+roshdology123@users.noreply.github.com>
2022-11-28 16:58:09 +0200

    Text Theme for Poppins

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:fongi_admin_portal/widgets/section_title.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sizer/sizer.dart';
 
