83088f98b85d31b99b2b3c3d96ef20725cbbc3ae
Abdallah Roshdy <67377062+roshdology123@users.noreply.github.com>
2022-11-29 13:58:38 +0200

    Screen for viewing the products

--- lib/screens/products_screen.dart
+++ lib/screens/products_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fongi_admin_portal/widgets/product_card.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:sizer/sizer.dart';
+
+import '../core/constants.dart';
 
 class ProductsScreen extends StatelessWidget {
   const ProductsScreen({Key? key}) : super(key: key);
@@ -13,6 +18,85 @@ class ProductsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Sizer(
+      builder: (BuildContext context, Orientation orientation,
+          DeviceType deviceType) {
+        return Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text(
+              'Products',
+              style: Theme.of(context)
+                  .textTheme
+                  .headline3!
+                  .copyWith(color: Colors.white),
+            ),
+            backgroundColor: kMainColor,
+          ),
+          body: Container(
+            color: kBackgroundColor,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    color: Colors.white,
+                    width: 92.w,
+                    height: 15.h,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.pushNamed(context, '/newProduct');
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Add new product',
+                            style: Theme.of(context).textTheme.headline3,
+                          ),
+                          const Icon(FontAwesomeIcons.plus),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const ProductCard(
+                      title: 'Lorem ipsum',
+                      description: 'Lorem ipsum dolor sit amet',
+                      imgPath: 'assets/images/NikeAirForce.webp'),
+                  const ProductCard(
+                      title: 'Lorem ipsum',
+                      description: 'Lorem ipsum dolor sit amet',
+                      imgPath: 'assets/images/NikeAirForce.webp'),
+                  const ProductCard(
+                      title: 'Lorem ipsum',
+                      description: 'Lorem ipsum dolor sit amet',
+                      imgPath: 'assets/images/NikeAirForce.webp'),
+                  const ProductCard(
+                      title: 'Lorem ipsum',
+                      description: 'Lorem ipsum dolor sit amet',
+                      imgPath: 'assets/images/NikeAirForce.webp'),
+                  const ProductCard(
+                      title: 'Lorem ipsum',
+                      description: 'Lorem ipsum dolor sit amet',
+                      imgPath: 'assets/images/NikeAirForce.webp'),
+                  const ProductCard(
+                      title: 'Lorem ipsum',
+                      description: 'Lorem ipsum dolor sit amet',
+                      imgPath: 'assets/images/NikeAirForce.webp'),
+                  const ProductCard(
+                      title: 'Lorem ipsum',
+                      description: 'Lorem ipsum dolor sit amet',
+                      imgPath: 'assets/images/NikeAirForce.webp'),
+                  const ProductCard(
+                      title: 'Lorem ipsum',
+                      description: 'Lorem ipsum dolor sit amet',
+                      imgPath: 'assets/images/NikeAirForce.webp'),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
